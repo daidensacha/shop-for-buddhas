9a3f33fcb56f80f3076135a42ee87c1b1f012aca
Daiden Sacha <daidensacha@gmail.com>
2021-11-23 16:48:35 +0000

    created git  branch for account app development

--- shop_for_buddhas/settings.py
+++ shop_for_buddhas/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+if os.path.exists('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7triiyz0&7i)$g5m2*ljrcl4n28vuarbhl^0^s*yp1_w&!@6e'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
