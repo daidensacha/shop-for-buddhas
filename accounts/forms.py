7a3118740d8f2ee61ee33ed9950dce9c0473e47b
Daiden Sacha <daidensacha@gmail.com>
2021-11-24 16:34:56 +0000

    Fixed issue with linking of static images. Minor imporvements and changes to display of account info in the admin panel

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,17 +4,17 @@ from .models import Profile
 
 
 user_type = (
-    ('customer', "Customer"),
-    ('vendor', "Vendor")
+    ('is_customer', "Customer"),
+    ('is_vendor', "Vendor")
 )
 
 
 class CustomSignup(SignupForm):
-    user_type = forms.ChoiceField(choices=user_type)
+    user_type = forms.ChoiceField(choices=user_type, required = True)
 
     def save(self, request):
         user = super(CustomSignup, self).save(request)
 
-        user.user_type=self.cleaned_data['user_type']
+        user.user_type = self.cleaned_data['user_type']
         user.save()
         return user
--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 user_type = (
-    ('customer', "Customer"),
-    ('vendor', "Vendor")
+    ('is_admin', "Admin"),
+    ('is_customer', "Customer"),
+    ('is_vendor', "Vendor")
 )
 
 
 class Profile(AbstractUser):
-    user_type = models.CharField(max_length=25, choices=user_type)
+    user_type = models.CharField(max_length=25, choices=user_type, blank = True)
