86832ffb7e9458ed10f38c056248f98aefe4c484
Daiden Sacha <daidensacha@gmail.com>
2021-11-29 12:56:56 +0000

    Added products view and html template

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Product, Category
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -12,15 +13,17 @@ class ProductAdmin(admin.ModelAdmin):
         'rating',
         'image',
     )
+    list_filter = ('category',)
+    ordering = ('category', 'sku',)
+    search_fields = ('name',)
 
-    ordering = ('sku',)
 
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Category, CategoryAdmin)
--- shop_for_buddhas/urls.py
+++ shop_for_buddhas/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('testimonials/', include('testimonials.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
