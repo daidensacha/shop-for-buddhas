4a1a698849389ace54ae375ef2e994899e6bbc8e
Daiden Sacha <daidensacha@gmail.com>
2021-12-07 15:30:16 +0000

    Updated shopping bag template

--- cart/views.py
+++ cart/views.py
@@ -18,29 +18,8 @@ def add_to_cart(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    # size = None
-    # if 'product_size' in request.POST:
-    #     size = request.POST['product_size']
     cart = request.session.get('cart', {})
 
-    # if size:
-    #     if item_id in list(cart.keys()):
-    #         if size in cart[item_id]['items_by_size'].keys():
-    #             cart[item_id]['items_by_size'][size] += quantity
-    #             messages.success(
-    #                 request, f'Updated size {size.upper()} ' +
-    #                          f'{product.name} quantity to ' +
-    #                          f'{cart[item_id]["items_by_size"][size]}')
-    #         else:
-    #             cart[item_id]['items_by_size'][size] = quantity
-    #             messages.success(
-    #                 request, f'Added size {size.upper()} ' +
-    #                          f'{product.name} to your cart')
-    #     else:
-    #         cart[item_id] = {'items_by_size': {size: quantity}}
-    #         messages.success(request, f'Added size {size.upper()} ' +
-    #                                   f'{product.name} to your cart')
-    # else:
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
         messages.success(request, f'Updated {product.name}' +
@@ -50,5 +29,4 @@ def add_to_cart(request, item_id):
         messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
