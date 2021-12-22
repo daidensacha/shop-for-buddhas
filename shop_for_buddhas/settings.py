5e9a82cf1d9adc48e4cf5f4ab8f0727ad64937d4
Daiden Sacha <daidensacha@gmail.com>
2021-12-22 17:57:52 +0000

    Disabled collect static in heroku. Add host name to settings.py

--- shop_for_buddhas/settings.py
+++ shop_for_buddhas/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['shop-for-buddhas.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -155,12 +154,24 @@ WSGI_APPLICATION = 'shop_for_buddhas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
