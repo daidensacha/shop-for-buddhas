3f1c5fa3afaaafbdcdf6c02a74212aacb2eafd55
Daiden Sacha <daidensacha@gmail.com>
2021-12-22 20:08:21 +0000

    Change DEBUG to 'development' if os environ

--- shop_for_buddhas/settings.py
+++ shop_for_buddhas/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # STRIPE_PRIVATE_KEY = os.environ.get('STRIPE_PRIVATE_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shop-for-buddhas.herokuapp.com', 'localhost']
 
