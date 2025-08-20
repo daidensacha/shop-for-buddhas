4203d1d13b990035864bfc2526aaf9024cf006e1
daidensacha <daidensacha@gmail.com>
2025-08-20 15:18:07 +0200

    docs: update readme

--- shop_for_buddhas/settings.py
+++ shop_for_buddhas/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['shop-for-buddhas.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['shop-for-buddhas.herokuapp.com']
+if DEBUG:
+    ALLOWED_HOSTS += ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
